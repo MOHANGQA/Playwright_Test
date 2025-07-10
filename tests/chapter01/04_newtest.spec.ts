413e21b0f819ce91469ebe52eb81a18106ecc899
MOHANGQA <mohanqatraining@gmail.com>
2025-07-10 22:12:43 +0530

    wait statement added

--- tests/chapter01/04_newtest.spec.ts
+++ tests/chapter01/04_newtest.spec.ts
@@ -8,5 +8,6 @@ test('playwright tutorial test', async({page}) => {
     await page.getByRole('combobox', { name: 'Search' }).fill('amazon.com');
     await page.getByRole('combobox', { name: 'Search' }).press('Enter');
 
+    await page.waitForTimeout(3000);
 
 })
