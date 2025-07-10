eb2e014819c289be6b9fab5e0365b1323ffd3ef7
MOHANGQA <mohanqatraining@gmail.com>
2025-07-10 21:03:12 +0530

    Added code in 04_newtest.spec.ts

---
+++ tests/chapter01/04_newtest.spec.ts
@@ -0,0 +1,12 @@
+
+import { test, Page, expect}from '@playwright/test'
+
+test('playwright tutorial test', async({page}) => {
+
+    await page.goto("https://www.google.com");
+
+    await page.getByRole('combobox', { name: 'Search' }).fill('amazon.com');
+    await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+
+
+})
